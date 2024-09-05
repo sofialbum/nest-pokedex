fe3b7e077dd3fb75c044fd8a2be2a42bc46aaff7
Sofía González <sofia_1.6@hotmail.com>
2024-09-05 19:51:32 -0300

    seedy paginación

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AxiosAdapter } from './http-adapters/axios.adapter';
 
-@Module({})
+@Module({
+  providers: [ AxiosAdapter],
+  exports: [ AxiosAdapter ]
+})
 export class CommonModule {}
--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -13,5 +13,6 @@ export class SeedController {
     return this.seedService.executeSeed();
   }
 
+  
 
 }
