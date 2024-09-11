7d8470e72a9855973fcbc49b4144097f2f4fb9ce
Sofía González <sofia_1.6@hotmail.com>
2024-09-11 14:48:10 -0300

    preparar mongodb y envs

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,7 +1,9 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
-import { InjectModel } from '@nestjs/mongoose';
 
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -9,13 +11,23 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 
 
+
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
-  ) {}
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly configService: ConfigService
+  ) {
+    
+   this.defaultLimit = configService.get<number>('defaultLimit');
+  //  console.log({defaultLimit: configService.get<number>('defaultLimit')})
+  
+  }
 
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -34,7 +46,7 @@ export class PokemonService {
 
   findAll( paginationDto: PaginationDto) {
 
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
     return this.pokemonModel.find()
       .limit( limit )
       .skip( offset )
